531f43d885384d61be0e2a8fc7eafb64c3466ee8
somename <someemail>
2015-10-23 11:59:22 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -33,8 +33,8 @@ public class PlanetExplorer {
 		
 		String position = "";
 		
-		if (command == "f")
-			pos_y--;
+//		if (command == "f")
+//			pos_y--;
 		
 		if (facing == 0)
 			position = "(" + pos_x + "," + pos_y + "," + "N" + ")";
